170ccaf2aea33ae6d8ccdb6f01d7d088b882a03d
Nickolas Eduardo <nickolasvieira.el@gmail.com>
2022-06-26 07:31:26 -0300

    HTML running with the server

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+
+app.set("view engine", "ejs")
+
+app.get("/", function(req, res) {
+    res.render('index')
+})
+
+app.get("/about", function(req, res) {
+    res.render('about')
+})
+
+const port = 8080
+app.listen(port)
+console.log(`Rodando na ${port}`)
